8b23b014d9f14535b1d65276d8e8986ec2e91bf6
Tim Allen <tiallen@softlayer.com>
2016-07-26 12:30:34 -0500

    Separate main app logic into module.

--- golerta.go
+++ golerta.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-  "fmt"
-
   "github.com/dgrijalva/jwt-go"
   jwtmiddleware "github.com/iris-contrib/middleware/jwt"
   "github.com/kataras/iris"
   "github.com/allen13/golerta/auth"
   "github.com/docopt/docopt-go"
   "github.com/allen13/golerta/config"
+  "github.com/allen13/golerta/app"
 )
 
 const version = "Golerta 0.0.1"
@@ -29,34 +28,15 @@ func main() {
   args, _ := docopt.Parse(usage, nil, true, version, false)
   configFile := args["--config"].(string)
   config := config.BuildConfig(configFile)
-  fmt.Println(config)
-
 
   golerta := iris.New()
 
   authorizationMiddleware := buildAuthorizationMiddleware(config.Golerta.SigningKey)
 
   auth.RegisterAuthProvider(config, golerta)
-  golerta.Get("/ping", pingHandler)
-  golerta.Get("/secured/ping", authorizationMiddleware.Serve, securedPingHandler)
+  app.RegisterApp(config,golerta,authorizationMiddleware)
 
   golerta.Listen(":8080")
-}
-
-type response struct {
-  Text string `json:"text"`
-}
-
-func pingHandler(ctx *iris.Context) {
-  response := response{"All good. You don't need to be authenticated to call this"}
-  ctx.JSON(iris.StatusOK, response)
-}
-
-func securedPingHandler(ctx *iris.Context) {
-  response := response{"All good. You only get this message if you're authenticated"}
-  // get the *jwt.Token which contains user information using:
-  // user:= myJwtMiddleware.Get(ctx) or context.Get("jwt").(*jwt.Token)
-  ctx.JSON(iris.StatusOK, response)
 }
 
 func buildAuthorizationMiddleware(signingKey string)(*jwtmiddleware.Middleware){
