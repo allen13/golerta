71682f11d90879ee1fa3737f26c6238efb9f8df6
Tim Allen <tiallen@softlayer.com>
2016-07-27 15:06:13 -0500

    Create alert model. Implement and test CRUD operations for alerts.

---
+++ db/rethinkdb_test.go
@@ -0,0 +1,68 @@
+package db
+
+import (
+  "testing"
+  "github.com/allen13/golerta/models"
+  r "gopkg.in/dancannon/gorethink.v2"
+)
+
+func TestRethinkDB_CRUDAlert(t *testing.T) {
+  db := getTestDB(t)
+
+  alert := &models.Alert{
+    Event: "cpu usage idle",
+    Resource: "testServer01",
+    Environment: "syd01",
+    Severity: "CRITICAL",
+    Origin: "consul-syd01",
+  }
+  alert.GenerateDefaults()
+
+  //Create Alert
+  id, err := db.CreateAlert(*alert)
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  //Update Alert and check that it was actually updated
+  alertUpdate := map[string]interface{}{
+    "duplicateCount": r.Row.Field("duplicateCount").Add(1),
+  }
+  err = db.UpdateAlert(id, alertUpdate)
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  //Get Alert and check that it is the same in addition to the duplciateCount field being updated
+  dbAlert, err := db.GetAlert(id)
+  if err != nil {
+    t.Fatal(err)
+  }
+  if alert.Event != dbAlert.Event && alert.DuplicateCount == dbAlert.DuplicateCount + 1{
+    t.Fatal("Failed to get back the correct alert")
+  }
+
+  //Delete Alert and check that it was deleted
+  err = db.DeleteAlert(id)
+  if err != nil {
+    t.Fatal(err)
+  }
+  _, err = db.GetAlert(id)
+  if err == nil {
+    t.Fatal("Alert was not properly deleted")
+  }
+}
+
+func getTestDB(t *testing.T)(db* RethinkDB){
+  db = &RethinkDB{
+    Address: "localhost:28015",
+    Database: "alerta",
+  }
+  err := db.Connect()
+
+  if err != nil{
+    t.Fatal(err)
+  }
+
+  return db
+}
