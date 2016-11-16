07659e955067b5292dd9c9f441721b815c1fbfcd
Tim Allen <tlallen@us.ibm.com>
2016-11-16 13:47:17 -0600

    Make golerta client side endpoint adjust to whatever the origin is.

--- static/config.js
+++ static/config.js
@@ -2,7 +2,7 @@
 
 angular.module('config', [])
   .constant('config', {
-    'endpoint'    : "http://"+window.location.hostname+":"+window.location.port,
+    'endpoint'    : window.location.origin,
     'provider'    : "basic", // google, github, gitlab or basic
     'client_id'   : "INSERT-CLIENT-ID-HERE",
     'gitlab_url'  : "https://gitlab.com",  // replace with your gitlab server
